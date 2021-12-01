1350ea5693e1a04026ffb0aec8a4abba14797704
TheKingDave <david@langheiter.com>
2021-12-01 23:54:19 +0100

    First level

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt").expect("intput.txt file not found");
+    let numbers: Vec<i32> = contents.lines().map(|s| s.parse().expect("NaN")).collect();
+    let mut count = 0;
+
+    for i in 1..numbers.len() {
+        if numbers[i-1] < numbers[i] {
+            count += 1;
+        }
+    }
+
+    print!("Output: {}", count);
 }
